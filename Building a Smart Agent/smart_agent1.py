a7b0d80d9433623fa4e8ec36d38103036f7c8aee
Steven Brown <superhappycamperboy@gmail.com>
2017-10-09 20:51:47 +1000

    Create smart_agent1.py

---
+++ Building a Smart Agent/smart_agent1.py
@@ -0,0 +1,83 @@
+import random
+import math
+
+import numpy as np
+import pandas as pd
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions
+from pysc2.lib import features
+
+_NO_OP = actions.FUNCTIONS.no_op.id
+_SELECT_POINT = actions.FUNCTIONS.select_point.id
+_BUILD_SUPPLY_DEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+_BUILD_BARRACKS = actions.FUNCTIONS.Build_Barracks_screen.id
+_TRAIN_MARINE = actions.FUNCTIONS.Train_Marine_quick.id
+_SELECT_ARMY = actions.FUNCTIONS.select_army.id
+_ATTACK_MINIMAP = actions.FUNCTIONS.Attack_minimap.id
+
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+_PLAYER_ID = features.SCREEN_FEATURES.player_id.index
+
+_PLAYER_SELF = 1
+
+_TERRAN_COMMANDCENTER = 18
+_TERRAN_SCV = 45 
+_TERRAN_SUPPLY_DEPOT = 19
+_TERRAN_BARRACKS = 21
+
+_SCREEN = [0]
+
+# Stolen from https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow
+class QLearningTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions  # a list
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon = e_greedy
+        self.q_table = pd.DataFrame(columns=self.actions)
+
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        
+        if np.random.uniform() < self.epsilon:
+            # choose best action
+            state_action = self.q_table.ix[observation, :]
+            
+            # some actions have the same value
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+            
+            action = state_action.argmax()
+        else:
+            # choose random action
+            action = np.random.choice(self.actions)
+            
+        return action
+
+    def learn(self, s, a, r, s_):
+        self.check_state_exist(s_)
+        self.check_state_exist(s)
+        
+        q_predict = self.q_table.ix[s, a]
+        q_target = r + self.gamma * self.q_table.ix[s_, :].max()
+        
+        # update
+        self.q_table.ix[s, a] += self.lr * (q_target - q_predict)
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            self.q_table = self.q_table.append(pd.Series([0] * len(self.actions), index=self.q_table.columns, name=state))
+
+class SmartAgent(base_agent.BaseAgent):
+    def transformLocation(self, x, x_distance, y, y_distance):
+        if not self.base_top_left:
+            return [x - x_distance, y - y_distance]
+        
+        return [x + x_distance, y + y_distance]
+        
+    def step(self, obs):
+        super(SmartAgent, self).step(obs)
+            
+        return actions.FunctionCall(_NO_OP, [])
