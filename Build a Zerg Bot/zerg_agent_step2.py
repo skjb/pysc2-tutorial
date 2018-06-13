f241c9fb37359840215deef506d2f1973ec86614
Steven Brown <superhappycamperboy@gmail.com>
2018-06-13 10:58:18 +1000

    Create zerg_agent_step2.py

---
+++ Build a Zerg Bot/zerg_agent_step2.py
@@ -0,0 +1,42 @@
+from pysc2.agents import base_agent
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features
+from absl import app
+
+class ZergAgent(base_agent.BaseAgent):
+  def step(self, obs):
+    super(ZergAgent, self).step(obs)
+    
+    return actions.FUNCTIONS.no_op()
+
+def main(unused_argv):
+  agent = ZergAgent()
+  try:
+    while True:
+      with sc2_env.SC2Env(
+          map_name="AbyssalReef",
+          players=[sc2_env.Agent(sc2_env.Race.zerg),
+                   sc2_env.Bot(sc2_env.Race.random,
+                               sc2_env.Difficulty.very_easy)],
+          agent_interface_format=features.AgentInterfaceFormat(
+              feature_dimensions=features.Dimensions(screen=84, minimap=64)),
+          step_mul=16,
+          game_steps_per_episode=0,
+          visualize=True) as env:
+          
+        agent.setup(env.observation_spec(), env.action_spec())
+        
+        timesteps = env.reset()
+        agent.reset()
+        
+        while True:
+          step_actions = [agent.step(timesteps[0])]
+          if timesteps[0].last():
+            break
+          timesteps = env.step(step_actions)
+      
+  except KeyboardInterrupt:
+    pass
+  
+if __name__ == "__main__":
+  app.run(main)
