5f35eac430e94700381ec6065c0271be4a75becd
Steven Brown <superhappycamperboy@gmail.com>
2018-06-13 11:04:15 +1000

    Create zerg_agent_step3.py

---
+++ Build a Zerg Bot/zerg_agent_step3.py
@@ -0,0 +1,51 @@
+from pysc2.agents import base_agent
+from pysc2.env import sc2_env
+from pysc2.lib import actions, features, units
+from absl import app
+import random
+
+class ZergAgent(base_agent.BaseAgent):
+  def step(self, obs):
+    super(ZergAgent, self).step(obs)
+    
+    drones = [unit for unit in obs.observation.feature_units
+              if unit.unit_type == units.Zerg.Drone]
+    if len(drones) > 0:
+      drone = random.choice(drones)
+      
+      return actions.FUNCTIONS.select_point("select_all_type", (drone.x,
+                                                                drone.y))
+    
+    return actions.FUNCTIONS.no_op()
+
+def main(unused_argv):
+  agent = ZergAgent()
+  try:
+    while True:
+      with sc2_env.SC2Env(
+          map_name="AbyssalReef",
+          players=[sc2_env.Agent(sc2_env.Race.zerg),
+                   sc2_env.Bot(sc2_env.Race.random,
+                               sc2_env.Difficulty.very_easy)],
+          agent_interface_format=features.AgentInterfaceFormat(
+              feature_dimensions=features.Dimensions(screen=84, minimap=64)),
+          step_mul=16,
+          game_steps_per_episode=0,
+          visualize=True) as env:
+          
+        agent.setup(env.observation_spec(), env.action_spec())
+        
+        timesteps = env.reset()
+        agent.reset()
+        
+        while True:
+          step_actions = [agent.step(timesteps[0])]
+          if timesteps[0].last():
+            break
+          timesteps = env.step(step_actions)
+      
+  except KeyboardInterrupt:
+    pass
+  
+if __name__ == "__main__":
+  app.run(main)
