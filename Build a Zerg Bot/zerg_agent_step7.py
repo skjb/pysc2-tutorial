029387378c2b321842e063bbbb7fbfce8f1ab0d1
Steven Brown <superhappycamperboy@gmail.com>
2018-06-13 11:47:37 +1000

    Update zerg_agent_step7.py

--- Build a Zerg Bot/zerg_agent_step7.py
+++ Build a Zerg Bot/zerg_agent_step7.py
@@ -97,7 +97,8 @@ def main(unused_argv):
                    sc2_env.Bot(sc2_env.Race.random,
                                sc2_env.Difficulty.very_easy)],
           agent_interface_format=features.AgentInterfaceFormat(
-              feature_dimensions=features.Dimensions(screen=84, minimap=64)),
+              feature_dimensions=features.Dimensions(screen=84, minimap=64),
+              use_feature_units=True),
           step_mul=16,
           game_steps_per_episode=0,
           visualize=True) as env:
