a0f4e3e01153058a037e8b6521e4c450cafd8b6b
Steven Brown <998458+skjb@users.noreply.github.com>
2020-03-11 11:34:18 +1000

    Update train_tvt.py

--- Battle Predictor/train_tvt.py
+++ Battle Predictor/train_tvt.py
@@ -2,7 +2,6 @@ import csv
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from joblib import dump, load
 
 xs = []
 ys = []
